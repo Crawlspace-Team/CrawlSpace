8beb87231be037da38df99f127eda54ccdc5fe7a
UP807639 <36421408+UP807639@users.noreply.github.com>
2018-02-16 15:49:00 +0000

    Adding all the required models
    Updating the Crawl model and adding all the other required models for the CrawlSpace database.

--- Crawlspace-app/crawlspace/core/models.py
+++ Crawlspace-app/crawlspace/core/models.py
@@ -5,8 +5,17 @@ from django.contrib.auth.models import User
 import datetime
 
 # Create your models here.
-class Crawls(models.Model):
+class Crawl(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    description = models.CharField(max_length=20)
-    details = models.CharField(max_length=50)
+    Crawl_Name = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
+
+class Pub(models.Model):
+    Pub_Name = models.CharField(max_length=255)
+    Places_ID = models.TextField
+
+
+class Pub_On_Crawl(models.Model):
+    pub = models.ForeignKey(Pub, on_delete=models.CASCADE)
+    crawl = models.ForeignKey(Crawl, on_delete=models.CASCADE)
+    position = models.IntegerField()
