091d77ee2f543ed4a94c6fd99c1760114a8d4bd0
Daniel Hearn <DanielHearn@users.noreply.github.com>
2018-02-22 20:36:57 +0000

    Add edit crawl menu html and js
    Added the edit crawl menu html and js that load the crawls details into the edit crawl form inputs.

--- Crawlspace-app/static/js/crawlSpace_home.js
+++ Crawlspace-app/static/js/crawlSpace_home.js
@@ -1,7 +1,10 @@
 window.onload = function(){
-  const newCrawlDateInput = document.querySelector('#newCrawlDate')
-  newCrawlDateInput.valueAsDate = new Date()
+  const newCrawlDateInput = document.querySelectorAll('.newCrawlDate')
+  newCrawlDateInput.forEach(input => input.valueAsDate = new Date())
 };
+
+let newCrawlMenuOpen = false
+let editCrawlMenuOpen = false
 
 const newCrawlMenu = document.querySelector('.newCrawlMenu')
 const newCrawlOverlay = newCrawlMenu.querySelector('.overlay')
@@ -10,6 +13,46 @@ newCrawlButton.addEventListener('click', toggleNewCrawlMenu)
 newCrawlOverlay.addEventListener('click', toggleNewCrawlMenu)
 
 function toggleNewCrawlMenu() {
-  newCrawlMenu.classList.toggle('menu--open')
+  newCrawlMenuOpen = !newCrawlMenuOpen
+  if (editCrawlMenuOpen == true) {
+    editCrawlMenu.classList.remove('menu--open')
+    newCrawlMenu.classList.remove('menu--open')
+    editCrawlMenuOpen = !editCrawlMenuOpen
+  } else {
+    newCrawlMenu.classList.toggle('menu--open')
+  }
+  newCrawlButton.classList.toggle('button--active')
+}
+
+const editCrawlButtons = document.querySelectorAll('.editCrawlButton')
+const editCrawlMenu = document.querySelector('.editCrawlMenu')
+const editCrawlOverlay = editCrawlMenu.querySelector('.overlay')
+editCrawlButtons.forEach(button => button.addEventListener('click', openEditCrawlMenu))
+editCrawlOverlay.addEventListener('click', closeEditCrawlMenu)
+
+function openEditCrawlMenu(e) {
+  e.preventDefault()
+  let crawlElement = e.target.parentNode.parentNode.parentNode
+  let crawlNameElement = crawlElement.querySelector('.details_name')
+  let crawlDateElement = crawlElement.querySelector('.details_startDate')
+  const crawlName = crawlNameElement.innerHTML
+  let editCrawlName = editCrawlMenu.querySelector('.editCrawlName')
+  let editCrawlDate = editCrawlMenu.querySelector('.editCrawlDate')
+  let crawlDate = crawlDateElement.innerHTML.split('/')
+  const crawlYear = crawlDate[2]
+  const crawlMonth = (crawlDate[1]-1)
+  const crawlDay = crawlDate[0]
+  crawlDate = new Date(crawlYear, crawlMonth, crawlDay);
+  editCrawlName.setAttribute('value', crawlName)
+  editCrawlDate.valueAsDate = crawlDate
+
+  editCrawlMenuOpen = !editCrawlMenuOpen
+  editCrawlMenu.classList.toggle('menu--open')
+  newCrawlButton.classList.toggle('button--active')
+}
+
+function closeEditCrawlMenu() {
+  editCrawlMenuOpen = !editCrawlMenuOpen
+  editCrawlMenu.classList.toggle('menu--open')
   newCrawlButton.classList.toggle('button--active')
 }
